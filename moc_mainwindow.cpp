90ebf2ff2e356f7bbaf9d926be7b563c643c1581
gcairone <caironegiuseppe100@gmail.com>
2024-08-07 10:20:27 +0200

    funzionante sul nuovo linux

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -1,67 +1,63 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "mainwindow.h"
-#include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.3. It"
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
-QT_BEGIN_MOC_NAMESPACE
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[13];
-    char stringdata0[260];
-};
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
-        - idx * sizeof(QByteArrayData)) \
-    )
-static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
-    {
-QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 21), // "on_playButton_clicked"
-QT_MOC_LITERAL(2, 33, 0), // ""
-QT_MOC_LITERAL(3, 34, 21), // "on_stopButton_clicked"
-QT_MOC_LITERAL(4, 56, 22), // "on_pauseButton_clicked"
-QT_MOC_LITERAL(5, 79, 19), // "iteration_animation"
-QT_MOC_LITERAL(6, 99, 22), // "on_trainButton_clicked"
-QT_MOC_LITERAL(7, 122, 9), // "iteration"
-QT_MOC_LITERAL(8, 132, 21), // "iteration_with_choice"
-QT_MOC_LITERAL(9, 154, 25), // "on_qtable_load_sp_clicked"
-QT_MOC_LITERAL(10, 180, 26), // "on_qtable_store_sp_clicked"
-QT_MOC_LITERAL(11, 207, 25), // "on_qtable_load_st_clicked"
-QT_MOC_LITERAL(12, 233, 26) // "on_qtable_store_st_clicked"
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
 
-    },
-    "MainWindow\0on_playButton_clicked\0\0"
-    "on_stopButton_clicked\0on_pauseButton_clicked\0"
-    "iteration_animation\0on_trainButton_clicked\0"
-    "iteration\0iteration_with_choice\0"
-    "on_qtable_load_sp_clicked\0"
-    "on_qtable_store_sp_clicked\0"
-    "on_qtable_load_st_clicked\0"
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "on_playButton_clicked",
+    "",
+    "on_stopButton_clicked",
+    "on_pauseButton_clicked",
+    "iteration_animation",
+    "on_trainButton_clicked",
+    "iteration",
+    "iteration_with_choice",
+    "on_qtable_load_sp_clicked",
+    "on_qtable_store_sp_clicked",
+    "on_qtable_load_st_clicked",
     "on_qtable_store_st_clicked"
-};
-#undef QT_MOC_LITERAL
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
 
-static const uint qt_meta_data_MainWindow[] = {
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
 
  // content:
-       8,       // revision
+      12,       // revision
        0,       // classname
        0,    0, // classinfo
       11,   14, // methods
@@ -71,18 +67,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // flags
        0,       // signalCount
 
- // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    0,   70,    2, 0x08 /* Private */,
-       4,    0,   71,    2, 0x08 /* Private */,
-       5,    0,   72,    2, 0x08 /* Private */,
-       6,    0,   73,    2, 0x08 /* Private */,
-       7,    0,   74,    2, 0x08 /* Private */,
-       8,    0,   75,    2, 0x08 /* Private */,
-       9,    0,   76,    2, 0x08 /* Private */,
-      10,    0,   77,    2, 0x08 /* Private */,
-      11,    0,   78,    2, 0x08 /* Private */,
-      12,    0,   79,    2, 0x08 /* Private */,
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    0,   83,    2, 0x08,    4 /* Private */,
+       6,    0,   84,    2, 0x08,    5 /* Private */,
+       7,    0,   85,    2, 0x08,    6 /* Private */,
+       8,    0,   86,    2, 0x08,    7 /* Private */,
+       9,    0,   87,    2, 0x08,    8 /* Private */,
+      10,    0,   88,    2, 0x08,    9 /* Private */,
+      11,    0,   89,    2, 0x08,   10 /* Private */,
+      12,    0,   90,    2, 0x08,   11 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -99,6 +95,41 @@ static const uint qt_meta_data_MainWindow[] = {
 
        0        // eod
 };
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_playButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_stopButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pauseButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'iteration_animation'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_trainButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'iteration'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'iteration_with_choice'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_qtable_load_sp_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_qtable_store_sp_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_qtable_load_st_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_qtable_store_st_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
@@ -123,16 +154,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     (void)_a;
 }
 
-QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
-    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
-    qt_meta_stringdata_MainWindow.data,
-    qt_meta_data_MainWindow,
-    qt_static_metacall,
-    nullptr,
-    nullptr
-} };
-
-
 const QMetaObject *MainWindow::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
@@ -141,7 +162,7 @@ const QMetaObject *MainWindow::metaObject() const
 void *MainWindow::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
         return static_cast<void*>(this);
     return QMainWindow::qt_metacast(_clname);
 }
@@ -157,10 +178,9 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 11;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         if (_id < 11)
-            *reinterpret_cast<int*>(_a[0]) = -1;
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
         _id -= 11;
     }
     return _id;
 }
 QT_WARNING_POP
-QT_END_MOC_NAMESPACE
